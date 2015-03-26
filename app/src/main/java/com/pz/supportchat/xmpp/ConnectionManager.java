214eef84b3d1e8469f090985fcfd235b65598fb5
Patryk Zabicki <patryk.zabicki@gmail.com>
2015-03-26 21:56:43 +0100

    investigating the rosters.

--- app/src/main/java/com/pz/supportchat/xmpp/ConnectionManager.java
+++ app/src/main/java/com/pz/supportchat/xmpp/ConnectionManager.java
@@ -11,9 +11,15 @@ import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.chat.Chat;
 import org.jivesoftware.smack.chat.ChatManager;
 import org.jivesoftware.smack.chat.ChatManagerListener;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.roster.Roster;
+import org.jivesoftware.smack.roster.RosterListener;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 
+import android.util.Log;
+
 import java.io.IOException;
+import java.util.Collection;
 
 public class ConnectionManager implements IChatManager {
 
@@ -35,6 +41,32 @@ public class ConnectionManager implements IChatManager {
                     chat.addMessageListener(postingMessageListener);
                     mChatObservable = Optional.of(chat);
                 }
+            }
+        });
+
+        final Roster roster = Roster.getInstanceFor(connection);
+        roster.addRosterListener(new RosterListener() {
+            @Override
+            public void entriesAdded(Collection<String> addresses) {
+
+            }
+
+            @Override
+            public void entriesUpdated(Collection<String> addresses) {
+
+            }
+
+            @Override
+            public void entriesDeleted(Collection<String> addresses) {
+
+            }
+
+            @Override
+            public void presenceChanged(Presence presence) {
+                Log.d(ConnectionManager.class.getSimpleName(), " new presence from: " + presence.getFrom());
+                final Presence bestPresence = roster.getPresence(presence.getFrom());
+                bestPresence.isAvailable();
+                bestPresence.getStatus();
             }
         });
     }
